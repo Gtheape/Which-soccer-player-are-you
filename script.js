63096977c7cae50995fdb7dc2570d5e411494116
Gael Luna <75498183+Gtheape@users.noreply.github.com>
2021-06-04 13:10:35 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".answer").click(function(){
+    let soccerplayer = $("input").val();
+    let calm = "1";
+    let ruthless = "2";
+    let flair = "3";
+    let hardworking = "4";
+console.log(soccerplayer);
+});
